6b377cd49f433178d62a03ac24612e4759beee96
Adam <adam.rurek@gmail.com>
2019-09-19 12:38:17 +0200

    Login validation

--- src/components/Home/Login/Login.js
+++ src/components/Home/Login/Login.js
@@ -3,10 +3,18 @@ import './Login.scss'
 import Navigation from '../../Navigation/Navigation';
 import{Link} from 'react-router-dom';
 
+
+const mailReg=/^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
+
 class Login extends Component {
     state={
         border1: "border4Button",
-        border2: ""
+        border2: "",
+        email: "",
+        password: "",
+        errorEmail: false,
+        errorPassword: false,
+        formSend: false
 
     };
 
@@ -18,6 +26,36 @@ class Login extends Component {
         this.setState({border1:"", border2: "border4Button"})
     };
 
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+        let email= this.state.email;
+        let password = this.state.password;
+        let formSend = true;
+
+        if(!mailReg.test(email)) {
+            this.setState({errorEmail: true});
+            formSend = false;
+        } else {
+            this.setState({errorEmail: false});
+        }
+        if(password.length < 6) {
+            this.setState({errorPassword: true});
+            formSend = false;
+        } else {
+            this.setState({errorPassword: false});
+        }
+
+        this.setState({formSend: formSend})
+    };
+
+    handleEmailChange = e => {
+        this.setState({email: e.target.value})
+    };
+
+    handlePasswordChange = e => {
+        this.setState({password: e.target.value})
+    };
 
     render() {
         return (
@@ -30,20 +68,23 @@ class Login extends Component {
                 <span>Zaloguj się</span>
                 <div className="decoration"></div>
             </div>
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
+                {this.state.formSend && ""}
                 <div className="login_details">
                     <label>
                         Email
-                        <input type="email" name="email"/>
+                        <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange}/>
+                        {this.state.errorEmail && <span className="error_feedback">Podany email jest nieprawidłowy!</span>}
                     </label>
                     <label>
                         Hasło
-                        <input type="password" name="password" />
+                        <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                        {this.state.errorPassword && <span className="error_feedback">Podane hasło jest za krótkie!</span>}
                     </label>
                 </div>
                 <div className="login_buttons">
                     <button className={this.state.border1} onClick={this.handleClickBorderOne}><Link to="/rejestracja"> Załóż konto</Link></button>
-                    <button className={this.state.border2} onClick={this.handleClickBorderTwo}>Zaloguj się</button>
+                    <input  type="submit" name="submit" value="Zaloguj się" className={this.state.border2} onClick={this.handleClickBorderTwo}/>
                 </div>
             </form>
             </div>
